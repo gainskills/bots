e99292e2b2b04aa399fa161f7d4686536eba34f5
it@protempo.com <it@protempo.com>
2020-06-17 16:21:33 +1200

    1. added x12 grammer by default 2. change docker to alpine

---
+++ src/bots/templatetags/botsfilters.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import os
+from django import template
+
+register = template.Library()
+
+@register.filter
+def shortpath(path):
+    if path:
+        terug = os.path.basename(path)
+        if terug:
+            return terug
+        else:
+            return '(file)'     #for soem cases there is no good filename.....
+    return ''
---
+++ src/bots/usersys/grammars/x12/x12.py
@@ -0,0 +1,7 @@
+from bots.botsconfig import *
+from .envelope import recorddefs,structure,nextmessage
+
+
+syntax = {
+        'version'    :  '00403',    #only for sending
+        }
